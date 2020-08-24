4e08412fdb5a1ae404b0cf8ca7de513ccbf1ff5b
Maks151702 <59136977+Maks151702@users.noreply.github.com>
2020-08-24 13:33:56 +0300

    Upgrade

--- Unityone/Assets/Scripts/LevelManager.cs
+++ Unityone/Assets/Scripts/LevelManager.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+	public Charater player;
+	public GameObject currentCheckpoint;
     // Start is called before the first frame update
     void Start()
     {
-        player = FindObjectType<Character>();
+        player = FindObjectOfType<Charater>();
     }
 
     public void RespawnPlayer(){
